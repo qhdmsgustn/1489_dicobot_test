b89e91d436ed6971c6a964c3baf03ea86c09afb8
qhdmsgustn <qhdmsgustn@naver.com>
2020-07-13 02:21:18 +0900

    1일차 디코봇 학습

--- dicobot.py
+++ dicobot.py
@@ -1,36 +1,47 @@
 import discord
+from discord.utils import get
+from discord.ext import commands
 
-class chatbot(discord.Client):
-    # 프로그램 처음 실행될때 구성
-    async def on_ready(self):
-        # 상태메시지
-        # 종류 3가지 : Game, Streaming, CustomAActivity
-        game = discord.Game("내용")
+token="NzMxNzk5MDg4MjQ4NTIwNzMy.XwswRw.x2pbekOinHCq4IcKzuuhEuI5yOE"
 
-        # 계정상태변경
-        # 온라인상태,game 중으로 변경
-        await client.change_presence(status=discord.Status.online, activity=game)
+client = commands.Bot(command_prefix = '!') 
+client.remove_command('help')
 
-        # 준비가 완료되면 콘솔창에 "Ready!"라고 표시
-        print("Ready")
+###봇 시작###
+@client.event
+async def on_ready():
+    game = discord.Game('Test bot')
+    await client.change_presence(status=discord.Status.online, activity=game)
+    print(f'{client.user} 에 로그인하였습니다!')
 
-    async def on_message(self,message):
-        #sender가 bot일경우 반응 x
-        if message.author.bot:
-            return None
-        
-        #message.content = messasge 내용
-        if message.content == "!바보":
-            #현재 채널 받아옴
-            channel = message.channel
-            #답변내용 구성
-            msg = "너도 바보"
-            #msg에 지정된 내용대로 메시지를 전송
-            await channel.send(msg)
-            return None
+###채팅방 지우기###
+@client.command(name="청소",pass_context=True)
 
-if __name__ == "__main__":
-    #객체 생성
-    client = chatbot()
-    #Token 값을 통해 로그인하고 봇 실행
-    client.run("토큰")
+async def clear(ctx,amount:int):
+    await ctx.channel.purge(limit=amount)
+@clear.error
+async def clear_error(ctx,error):
+    if isinstance(error, commands.MissingRequiredArgument):
+       await ctx.send('숫자를 입력해주세요.')
+
+###명령어(대화)###
+
+@client.command(aliases=['안녕','안녕하세요', 'ㅎㅇ','hi', 'HI','Hello','HELLO'])
+async def hello(ctx):
+    await ctx.send(f"{ctx.author.mention}님 안녕하세요")
+
+@client.command()
+async def 망포(ctx):
+    await ctx.send("앞잡이")
+
+@client.command() 
+async def 바보(ctx):
+    await ctx.send("너도")
+
+###DM보내기###
+@client.command(name="DM보내기", pass_context=True)
+async def send_dm(ctx,user_name: discord.Member):
+    channel=await user_name.create_dm()
+    await channel.send("Hi~너에게 칭호를 주었어")
+
+client.run(token)
