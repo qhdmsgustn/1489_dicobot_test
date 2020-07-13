bb355357d1b2678e1dc1081f2c9e0063eccc3e23
qhdmsgustn <qhdmsgustn@naver.com>
2020-07-13 21:18:01 +0900

    통역기능 작동 ver 0.1

--- dicobot.py
+++ dicobot.py
@@ -1,9 +1,11 @@
 import discord
+from googletrans import Translator
 from discord.utils import get
 from discord.ext import commands
 
-token="토큰을 넣으세요"
+token="TOKEN"
 
+#명령어
 client = commands.Bot(command_prefix = '!') 
 client.remove_command('help')
 
@@ -14,15 +16,21 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=game)
     print(f'{client.user} 에 로그인하였습니다!')
 
-###채팅방 지우기###
-@client.command(name="청소",pass_context=True)
+###번역기###
+@client.command(name="!",pass_context=True)
+async def change_text(ctx,*,to_language):
+#번역과정
+    translator = Translator()
 
-async def clear(ctx,amount:int):
-    await ctx.channel.purge(limit=amount)
-@clear.error
-async def clear_error(ctx,error):
-    if isinstance(error, commands.MissingRequiredArgument):
-       await ctx.send('숫자를 입력해주세요.')
+    if translator.detect(to_language).lang=="ko":
+        temp_language = translator.translate(to_language,dest='en')
+        from_language = temp_language.text
+    else:
+        temp_language = translator.translate(to_language,dest='ko')
+        from_language = temp_language.text
+#출력  
+    await ctx.send(f"{ctx.author.mention}"+from_language)
+
 
 ###명령어(대화)###
 
@@ -30,18 +38,22 @@ async def clear_error(ctx,error):
 async def hello(ctx):
     await ctx.send(f"{ctx.author.mention}님 안녕하세요")
 
-@client.command()
-async def 망포(ctx):
-    await ctx.send("앞잡이")
-
-@client.command() 
-async def 바보(ctx):
-    await ctx.send("너도")
-
-###DM보내기###
-@client.command(name="DM보내기", pass_context=True)
-async def send_dm(ctx,user_name: discord.Member):
-    channel=await user_name.create_dm()
-    await channel.send("Hi~너에게 칭호를 주었어")
 
 client.run(token)
+
+
+###DM보내기###
+#@client.command(name="DM보내기", pass_context=True)
+#async def send_dm(ctx,user_name: discord.Member):
+#    channel=await user_name.create_dm()
+#    await channel.send("Hi~너에게 칭호를 주었어")
+
+###채팅방 지우기###
+#@client.command(name="청소",pass_context=True)
+#async def clear(ctx,amount:int):
+#    await ctx.channel.purge(limit=amount)
+
+#@clear.error
+#async def clear_error(ctx,error):
+#    if isinstance(error, commands.MissingRequiredArgument):
+#       await ctx.send('숫자를 입력해주세요.')
